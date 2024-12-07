8bc2bfd69d71097466fc8c6cdc7803e8def6df56
SoloDuck <colesjulius2@gmail.com>
2024-12-07 13:42:33 +0800

    feat: collision detection

--- src/entities.py
+++ src/entities.py
@@ -122,41 +122,24 @@ class Unit(GameObject):
                 travel_distance = self.speed * (dt / 1000)
                 self.x += (dx / distance) * travel_distance
                 self.y += (dy / distance) * travel_distance
-                self.rect.topleft = (int(self.x), int(self.y))  # Ensure integer coordinates for rect
+                self.rect.topleft = (self.x, self.y)
         elif self.destination:  # Move towards destination even if no target
             dx = self.destination[0] - self.x
-            if abs(dx) < 1:
-                self.x = self.destination[0]
             dy = self.destination[1] - self.y
-            if abs(dy) < 1:
-                self.y = self.destination[1]
             distance = math.hypot(dx, dy)
-
 
             if distance > 0:
                 travel_distance = self.speed * (dt / 1000)
-                # Adjust position if colliding with another unit
-                new_x = self.x + (dx / distance) * travel_distance
-                new_y = self.y + (dy / distance) * travel_distance
-                temp_rect = self.rect.copy()
-                temp_rect.topleft = (int(new_x), int(new_y))
-
-                collided_with_unit = check_collision_with_unit(temp_rect, units + enemies, exclude_unit=self)
-                if collided_with_unit:
-                    # Stop moving if collision detected
-                    self.destination = None
-                    return
 
                 if distance <= travel_distance:
-
                     self.x = self.destination[0]
                     self.y = self.destination[1]
-                    self.destination = None  # Stop moving when reaching the destination
+                    self.destination = None
                 else:
-                    self.x = new_x  # Update position
-                    self.y = new_y
+                    self.x += (dx / distance) * travel_distance
+                    self.y += (dy / distance) * travel_distance
 
-                self.rect.topleft = (int(self.x), int(self.y))
+                self.rect.topleft = (self.x, self.y)
 
 
     def handle_attack(self, dt, game_messages):
